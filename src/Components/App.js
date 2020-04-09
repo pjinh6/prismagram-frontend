f82624851a28e1d3ee14732f023c25f44c92eb13
JinhoPark <pjinh6@gmail.com>
2020-04-10 00:09:46 +0900

    Apollo Client

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from 'react-apollo-hooks';
 
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import Router from './Router';
+import Client from '../Apollo/Client';
 	
 export default () => (
 	<ThemeProvider theme={ Theme }>
-		<>
+		<ApolloProvider client={ Client }>
 			<GlobalStyles />
 			<Router isLoggedIn={ !false } />
-		</>
+		</ApolloProvider>
 	</ThemeProvider>
 );
